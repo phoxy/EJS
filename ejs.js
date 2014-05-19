4aaa09cda29040455950a4dee71e4d770345593b
Kirill Berezin <enelar@develop-project.ru>
2014-05-19 14:14:32 +0000

    Move utils functions to down

--- ejs.js
+++ ejs.js
@@ -1,47 +1,6 @@
 (function()
 {
-  var rsplit = function(string, regex)
-  {
-    var 
-      result = regex.exec(string),
-      retArr = new Array(),
-      first_idx, 
-      last_idx, 
-      first_bit;
-
-    while (result != null)
-    {
-      first_idx = result.index;
-      last_idx = regex.lastIndex;
-
-      if ((first_idx) != 0)
-      {
-        first_bit = string.substring(0,first_idx);
-        retArr.push(string.substring(0,first_idx));
-        string = string.slice(first_idx);
-      }    
-
-      retArr.push(result[0]);
-      string = string.slice(result[0].length);
-      result = regex.exec(string);  
-    }
-    
-    if (!string == '')
-      retArr.push(string);
-    return retArr;
-  };
-
-  var chop =  function(string)
-  {
-    return string.substr(0, string.length - 1);
-  };
-
-  var extend = function(d, s)
-  {
-    for(var n in s)
-      if(s.hasOwnProperty(n))
-        d[n] = s[n]
-  }
+  var rsplit, chop, extend;
 
   EJS = function( options )
   {
@@ -737,5 +696,49 @@
     
     request.open(params.method, params.url)
     request.send(null)
+  }
+
+// Function utils
+  var rsplit = function(string, regex)
+  {
+    var 
+      result = regex.exec(string),
+      retArr = new Array(),
+      first_idx, 
+      last_idx, 
+      first_bit;
+
+    while (result != null)
+    {
+      first_idx = result.index;
+      last_idx = regex.lastIndex;
+
+      if ((first_idx) != 0)
+      {
+        first_bit = string.substring(0,first_idx);
+        retArr.push(string.substring(0,first_idx));
+        string = string.slice(first_idx);
+      }    
+
+      retArr.push(result[0]);
+      string = string.slice(result[0].length);
+      result = regex.exec(string);  
+    }
+    
+    if (!string == '')
+      retArr.push(string);
+    return retArr;
+  };
+
+  var chop =  function(string)
+  {
+    return string.substr(0, string.length - 1);
+  };
+
+  var extend = function(d, s)
+  {
+    for(var n in s)
+      if(s.hasOwnProperty(n))
+        d[n] = s[n]
   }
 })();
